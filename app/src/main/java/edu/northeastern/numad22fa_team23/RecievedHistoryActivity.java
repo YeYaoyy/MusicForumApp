4e9795b7ff1d44d14c047fdee09d8dc17c25ef06
Zhuofan Wang <sunnyfighterwzf@gmail.com>
2022-11-06 23:19:19 -0800

    recieved history updated

---
+++ app/src/main/java/edu/northeastern/numad22fa_team23/RecievedHistoryActivity.java
@@ -0,0 +1,22 @@
+package edu.northeastern.numad22fa_team23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+
+import edu.northeastern.numad22fa_team23.R;
+
+
+public class RecievedHistoryActivity extends AppCompatActivity {
+    private DatabaseReference myDataBase;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recieved_history);
+    }
+}
